0a5c8d74d6777a772674bfbb93c903a310724f8a
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-04-15 15:51:00 +0300

    infinite-scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { type FC } from "react"
 import { Container, Header, List } from "./components"
+import useInfiniteScroll from "./hooks/useInfiniteScroll"
 
 const App: FC = () => {
+    const { loadMoreRef, page, } = useInfiniteScroll()
 
     return (
         <>
+            <div id="scrollArea"></div>
             <Header />
             <Container>
                 <List/>
             </Container>
+            <div ref={loadMoreRef}>SSS</div>
         </>
     )
 }
